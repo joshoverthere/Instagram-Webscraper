707c5608bd584d217e28e2c47d53f30751912352
joshoverthere <contact@xeta.space>
2022-01-24 22:23:35 +0000

    Implicit wait for cookies popup.

--- Instagram_Webscraper.py
+++ Instagram_Webscraper.py
@@ -9,16 +9,24 @@ import matplotlib.pyplot as plt
 
 class Insta_Scraper:
     def __init__(self, username, password):
+        #initiate webdriver even if it means reinstalling it
         self.driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+        #set object's logins
         self.username = username
         self.password = password
 
     def openGram(self):
+        #fetch instagram login page and wait for it to load
         self.driver.get("http://www.instagram.com")
 
         element_present = EC.presence_of_element_located((By.XPATH, '/html/body'))
         WebDriverWait(self.driver, 10).until(element_present)
         print("Fetched instagram login page.")
+
+        #wait for cookies popup to appear
+        element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[4]/div/div/button[1]'))
+        WebDriverWait(self.driver, 3).until(element_present)
+        sleep(100)
 
 
 def main():
